c636bdd2611b543c6f30b5c2d64440076556826b
Steve Hoffmann <stevehoff1777@gmail.com>
2023-09-22 18:07:48 -0700

    updated startup script

--- mailchimp_newzware.sh
+++ mailchimp_newzware.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-source /usr/local/rvm/environments/ruby-2.5.1@mailchimp
+source /usr/local/rvm/environments/ruby-3.0.0@mailchimp
 
 echo "$(date +%m/%d/%y\ %T)"
 cd /u/apps/mailchimp_newzware
 
-echo "wallowa"
 ruby import_subscribers.rb
 
 echo "$(date +%m/%d/%y\ %T)"
