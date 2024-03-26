a09d1f8c83f91b0b37e3edde3a4a90873f488549
Steve Hoffmann <stevehoff1777@gmail.com>
2024-03-26 08:50:04 -0700

    split export script into one for each site

---
+++ mailchimp_newzware_DA.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+source /usr/local/rvm/environments/ruby-3.0.0@mailchimp
+export PATH="/usr/local/rvm/gems/ruby-3.0.0@mailchimp/bin:$PATH"
+
+echo "$(date +%m/%d/%y\ %T)"
+cd /u/apps/mailchimp_newzware
+
+ruby import_subscribers.rb -site=DA -days=3 -logs=detail
+
+echo "$(date +%m/%d/%y\ %T)"
+echo "Export_Complete"
